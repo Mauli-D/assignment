2b196e7911dc77aa9cb65e47568da571ffcbe0fe
Rutulpatel7077 <rutulpatel7077@gmail.com>
2018-03-11 20:51:00 -0400

    done

---
+++ models/Struct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_200383477.models
+{
+    class Struct
+    {
+
+
+        public struct Mouse
+        {
+            public string s;
+            public readonly string value1;
+            public readonly string value2;
+
+            public Mouse(string value1, string value2, string value3) : this() => new Mouse() { s = value1 + value2 + value3 };
+
+        }
+
+    }
+}
